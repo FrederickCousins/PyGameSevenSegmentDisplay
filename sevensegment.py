7d251c3bdd15b17dd8a2835d9a3fe417e58c6f63
FrederickCousins <fpcousins@outlook.com>
2024-01-17 16:33:21 +0000

    add off segment behaviour and update test.py to include it

--- sevensegment.py
+++ sevensegment.py
@@ -18,63 +18,63 @@ E = 0x10
 F = 0x20
 G = 0x40
 
-def segments(*args):
+def on_segments(*args):
     '''
     takes the segment codes of segments that must be ON, and returns an
     appropriate int by logically ORing all the supplied codes
     '''
     return reduce(lambda x, y: (x | y), args, 0)
 
-ALL = segments(A, B, C, D, E, F, G)
+ALL = on_segments(A, B, C, D, E, F, G)
 
 # Sometimes, it is nicer to specify which segments should be off.
 def off_segments(*args):
     '''
     takes the segments that are to remain off, and returns an appropriate value
     '''
-    return (ALL & (~ segments(*args)))
+    return (ALL & (~ on_segments(*args)))
     
         
 # This map could as well have used sets, but I decided to stick to bit twiddling.
 SSD_CHAR_MAP = {
-                '0': segments(A, B, C, D, E, F),
-                '1': segments(B, C),
-                '2': segments(A, B, D, E, G),
-                '3': segments(A, B, C, D, G),
-                '4': segments(B, C, G, F),
-                '5': segments(A, C, D, F, G),
-                '6': segments(A, C, D, E, F, G),
-                '7': segments(A, B, C),
-                '8': segments(A, B, C, D, E, F, G),
-                '9': segments(A, B, C, D, F, G),
+                '0': on_segments(A, B, C, D, E, F),
+                '1': on_segments(B, C),
+                '2': on_segments(A, B, D, E, G),
+                '3': on_segments(A, B, C, D, G),
+                '4': on_segments(B, C, G, F),
+                '5': on_segments(A, C, D, F, G),
+                '6': on_segments(A, C, D, E, F, G),
+                '7': on_segments(A, B, C),
+                '8': on_segments(A, B, C, D, E, F, G),
+                '9': on_segments(A, B, C, D, F, G),
                 
                  # SPACE
                 ' ': off_segments(A, B, C, D, E, F, G),
                 
                 'A': off_segments(D),
                 'B': off_segments(A, B),
-                'C': segments(A, D, E, F),
+                'C': on_segments(A, D, E, F),
                 'D': off_segments(A, F),
                 'E': off_segments(B, C),
                 'F': off_segments(B, C, D),
                 'G': off_segments(B, G),
                 'H': off_segments(A, B, D),
-                'I': segments(B, C),
+                'I': on_segments(B, C),
                 'J': off_segments(A, F, G),
                 # K looks like a capital H, but that's the best we can do with
                 # an SSD.
                 'K': off_segments(A, D),
-                'L': segments(D, E, F),
+                'L': on_segments(D, E, F),
                  # write two Ns if you need an M.
                 'N': off_segments(D, G),
                 'O': off_segments(A, B, F),
                 'P': off_segments(C, D),
                 'Q': off_segments(D, E),
-                'R': segments(E, G),
+                'R': on_segments(E, G),
                 'S': off_segments(B, E),
                 'T': off_segments(A, B, C),
                 'U': off_segments(A, G),
-                'V': segments(C, D, E), # Actually looks like a u.
+                'V': on_segments(C, D, E), # Actually looks like a u.
                  # W: use two Vs
                  # No X 
                 'Y': off_segments(A, E),
@@ -91,7 +91,8 @@ class SevenSegmentChar(object):
         
     def __init__(self,
                  char,
-                 colour=(255, 0, 0),
+                 colour_on=(255, 0, 0),
+                 colour_off=(40,0,0),
                  bgcolour=(0,0,0), 
                  width=7, 
                  height=17,
@@ -100,7 +101,8 @@ class SevenSegmentChar(object):
         ctor;
         takes the following args:
         char: The char to represent. This value must be a key in SSD_CHAR_MAP.
-        colour: (R,G,B) colour tuple - default red.
+        colour_on: (R,G,B) colour tuple - default red.
+        colour_off: (R,G,B) colour tuple - default dark red.
         bgcolour: (R,G,B) triple for the background of this char - default black.
         width: width of this character in pixels - default is 7px.
         height: height of this character in pixels - default is 17px.
@@ -109,7 +111,8 @@ class SevenSegmentChar(object):
         assert char in SSD_CHAR_MAP
         self._char = char
         self._width = width
-        self._colour = colour
+        self._colour_on = colour_on
+        self._colour_off = colour_off
         self._bgcolour = bgcolour
         self._segment_width = segment_width
         self._segment_length = self._width - 2 * self._segment_width
@@ -117,19 +120,30 @@ class SevenSegmentChar(object):
         self._surface = pygame.Surface((self._width, self._height))
         self._surface.fill(bgcolour)
         
-        segment_point_pairs = self._get_segment_points()
-        for point_pair in segment_point_pairs:
+        segment_on_point_pairs = self._get_on_segment_points()
+        for point_pair in segment_on_point_pairs:
             print(point_pair)
             if point_pair:
                 pygame.draw.line(self._surface,
-                                 self._colour,
+                                 self._colour_on,
+                                 point_pair[0], 
+                                 point_pair[1],
+                                 self._segment_width
+                               )
+        
+        segment_off_point_pairs = self._get_off_segment_points()
+        for point_pair in segment_off_point_pairs:
+            print(point_pair)
+            if point_pair:
+                pygame.draw.line(self._surface,
+                                 self._colour_off,
                                  point_pair[0], 
                                  point_pair[1],
                                  self._segment_width
                                )
         
         
-    def _get_segment_points(self):
+    def _get_on_segment_points(self):
         '''
         returns an iterable of point pairs which denote the start and end points
         of each segment, in order from A to G. A value of None, [], or () should 
@@ -176,14 +190,61 @@ class SevenSegmentChar(object):
         
         return ret
     
+    def _get_off_segment_points(self):
+        '''
+        returns an iterable of point pairs which denote the start and end points
+        of each segment, in order from A to G. A value of None, [], or () should 
+        be treated as a lit segment.
+        '''
+        xmin = self._segment_width
+        xmax = self._segment_width + self._segment_length
+        
+        ymin = self._segment_width
+        ymax = 2 * self._segment_length + 2 * self._segment_width
+         
+        slen = self._segment_length
+        
+        positions = {
+                     A: ((xmin, ymin),
+                         (xmax, ymin)),
+                     
+                     B: ((xmax, ymin),
+                         (xmax, ymin + slen)),
+                     
+                     C: ((xmax, ymax - slen),
+                         (xmax, ymax)),
+                     
+                     D: ((xmax, ymax),
+                         (xmin, ymax)),
+                     
+                     E: ((xmin, ymax), 
+                         (xmin, ymax - slen)),
+                     
+                     F: ((xmin, ymin),
+                         (xmin, ymin + slen)),
+                     
+                     G: ((xmin, ymin + slen),
+                         (xmax, ymin + slen))}
+        
+        ssd = SSD_CHAR_MAP[self._char]
+        ret = []
+        
+        for segment in (A, B, C, D, E, F, G):
+            if not segment & ssd:
+                ret.append(positions[segment])
+            else:
+                ret.append(None)
+        
+        return ret
+    
     def update(self):
         self._segment_length = self._width - 2 * self._segment_width
         self._surface.fill(self._bgcolour)
-        segment_point_pairs = self._get_segment_points()
+        segment_point_pairs = self._get_on_segment_points()
         for point_pair in segment_point_pairs:
             if point_pair:
                 pygame.draw.line(self._surface,
-                                 self._colour,
+                                 self._colour_on,
                                  point_pair[0], 
                                  point_pair[1],
                                  self._segment_width
@@ -208,12 +269,21 @@ class SevenSegmentChar(object):
         self.update()
         
     @property
-    def colour(self):
-        return self._colour
+    def colour_on(self):
+        return self._colour_on
     
-    @colour.setter
-    def colour(self, new):
-        self._colour = new
+    @colour_on.setter
+    def colour_on(self, new):
+        self._colour_on = new
+        self.update()
+
+    @property
+    def colour_off(self):
+        return self._colour_off
+    
+    @colour_on.setter
+    def colour_off(self, new):
+        self._colour_off = new
         self.update()
     
     @property
@@ -261,7 +331,8 @@ class SevenSegmentDisplay(list):
                  width,
                  height,
                  content='',
-                 colour=(255,0,0), # red
+                 colour_on=(255,0,0), # red
+                 colour_off=(40,0,0), # dark red
                  bgcolour=(0,0,0), # black
                  segment_width=2, 
                  char_width=None, 
@@ -275,7 +346,9 @@ class SevenSegmentDisplay(list):
             
             content: a string representing the content of the panel.
             
-            colour: colour of the lit up segments. default red.
+            colour_on: colour of the lit up segments. default red.
+
+            colour_off: colour of the off segments. default dark red.
             
             bgcolour: background of the display. default black.
             
@@ -292,7 +365,8 @@ class SevenSegmentDisplay(list):
         self._width = width
         self._height = height
         self._content = content
-        self._colour = colour
+        self._colour_on = colour_on
+        self._colour_off = colour_off
         self._bgcolour = bgcolour
         self._segment_width = segment_width
         self._right_justified = right_justify
@@ -314,7 +388,8 @@ class SevenSegmentDisplay(list):
         self._numchars = int(self._width / self._char_width)
         for c in self._content:
             self.append(SevenSegmentChar(c,
-                                         self._colour,
+                                         self._colour_on,
+                                         self._colour_off,
                                          self._bgcolour,
                                          self._char_width,
                                          self._height, 
@@ -362,14 +437,26 @@ class SevenSegmentDisplay(list):
         self.update()
     
     @property
-    def colour(self):
-        return self._colour
+    def colour_on(self):
+        return self._colour_on
     
-    @colour.setter
-    def colour(self, new):
-        self._colour = new
+    @colour_on.setter
+    def colour_on(self, new):
+        self._colour_on = new
         for ssdchar in self:
-            ssdchar.colour = new
+            ssdchar.colour_on = new
+        
+        self.update_surface()
+
+    @property
+    def colour_off(self):
+        return self._colour_off
+    
+    @colour_off.setter
+    def colour_off(self, new):
+        self._colour_off = new
+        for ssdchar in self:
+            ssdchar.colour_off = new
         
         self.update_surface()
     
